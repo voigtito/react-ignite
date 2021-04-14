88dac9201476b1a7ff1944294d11e7977775402b
voigtito <voigt.gustavoh@gmail.com>
2021-04-14 19:14:06 -0300

    Setting up the home page with styles

--- applications/ignews/src/pages/index.tsx
+++ applications/ignews/src/pages/index.tsx
@@ -3,10 +3,20 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Início | ig.news</title>
-    </Head>
-    <h1 className={styles.title}>Hello</h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl codding"/>
+      </main>
     </>
   )
 }
