fd48191bb37c32eb9fc992e65a7ca66f8cae3aa1
voigtito <voigt.gustavoh@gmail.com>
2021-04-14 18:30:32 -0300

    Ignews first commit

---
+++ applications/ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
+    <h1 className={styles.title}>Hello</h1>
+    </>
+  )
+}
