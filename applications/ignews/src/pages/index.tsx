26df342e5380d29fdcdfb0656da57356c5a4fbf3
voigtito <voigt.gustavoh@gmail.com>
2021-04-14 20:10:24 -0300

    Adding stripe payment subscribe

--- applications/ignews/src/pages/index.tsx
+++ applications/ignews/src/pages/index.tsx
@@ -1,6 +1,17 @@
 import styles from '../styles/home.module.scss';
 import Head from 'next/head';
-export default function Home() {
+import { GetStaticProps } from 'next';
+import {SubscribeButton} from '../components/SubscribeButton'
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -12,11 +23,33 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl codding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IgHUlAQXinyD3QzLi6IAQEy', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100)),
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
