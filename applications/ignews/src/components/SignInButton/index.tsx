8efd281cf77302e19004b328a62df18365f9a381
voigtito <voigt.gustavoh@gmail.com>
2021-04-15 20:33:07 -0300

    Added routes api with faunaDB with auth

--- applications/ignews/src/components/SignInButton/index.tsx
+++ applications/ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
+    return session ? (
+        <button type="button" className={styles.signInButton} onClick={() => signOut()}>
             <FaGithub color="#04d361" />
-            Gustavo Voigt
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
