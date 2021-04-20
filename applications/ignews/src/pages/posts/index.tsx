564e85a6c137fb05c944778e799111685b1fcb07
voigtito <voigt.gustavoh@gmail.com>
2021-04-20 08:24:01 -0300

    Finished post page

--- applications/ignews/src/pages/posts/index.tsx
+++ applications/ignews/src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Prismic from '@prismicio/client';
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 type Post = {
     slug: string;
@@ -27,11 +28,13 @@ export default function Posts({ posts }: PostsProps) {
                 <div className={styles.posts}>
                     {
                         posts.map(post => (
-                            <a key={post.slug} href='#'>
-                                <time>{post.updatedAt}</time>
-                                <strong>{post.title}</strong>
-                                <p>{post.excerpt}</p>
-                            </a>
+                            <Link key={post.slug} href={`/posts/${post.slug}`}>
+                                <a>
+                                    <time>{post.updatedAt}</time>
+                                    <strong>{post.title}</strong>
+                                    <p>{post.excerpt}</p>
+                                </a>
+                            </Link>
                         ))
                     }
                 </div>
