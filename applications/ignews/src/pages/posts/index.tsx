407cb89afcb43cb5cad55d356977157960446fd4
voigtito <voigt.gustavoh@gmail.com>
2021-04-20 07:58:42 -0300

    ActiveLink with cloneElement from react

--- applications/ignews/src/pages/posts/index.tsx
+++ applications/ignews/src/pages/posts/index.tsx
@@ -65,8 +65,6 @@ export const getStaticProps: GetStaticProps = async () => {
         };
     })
 
-    console.log(JSON.stringify(response, null, 2));
-
     return {
         props: {
             posts
