4d9a9068ebdef566c798cdeac43a5514e2947882
voigtito <voigt.gustavoh@gmail.com>
2021-04-26 07:35:52 -0300

    Sign in page with chakra

--- applications/dashgo/src/pages/index.tsx
+++ applications/dashgo/src/pages/index.tsx
@@ -1,5 +1,41 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>dashcarobono</h1>
+    <Flex w="100vw0" h="100vh" align="center" justify="center">
+      <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
+        <Stack spacing="4">
+          {/* FormControl is used for the label and input. So they wont receive the spacing from the Stack comp. */}
+          {/* All elements with action properties has _ in front of it. Like the _hover property */}
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
