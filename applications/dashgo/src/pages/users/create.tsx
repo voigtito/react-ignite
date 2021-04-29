f561d16456e673260f48510d08ceba0ac77ab61e
voigtito <voigt.gustavoh@gmail.com>
2021-04-29 07:41:08 -0300

    Added pagination and users pages

---
+++ applications/dashgo/src/pages/users/create.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex, Heading, Divider, VStack, HStack, SimpleGrid, Button, Icon, Table, Thead, Tr, Th, Td, Tbody, Checkbox, Text } from '@chakra-ui/react';
+
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+import { Input } from '../../components/Form/Input';
+
+export default function UserCreate() {
+    return (
+        <Box>
+            <Header />
+            <Flex w="100%" maxWidth={1480} mx="auto" px="6" mt="4">
+                <Sidebar />
+                <Box flex="1" borderRadius={8} p="8" bg="gray.800">
+                    <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+                    <Divider my="6" borderColor="gray.700" />
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                            <Input name="name" label="Nome completo" />
+                            <Input name="email" label="E-mail" />
+                        </SimpleGrid>
+                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                            <Input name="password" label="Senha" />
+                            <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+                        </SimpleGrid>
+                    </VStack>
+                    <Flex mt="8" justify="flex-end">
+                        <HStack spacing="4">
+                            <Button colorScheme="whiteAlpha">Cancelar</Button>
+                            <Button colorScheme="pink">Salvar</Button>
+                        </HStack>
+                    </Flex>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
--- applications/dashgo/src/pages/users/index.tsx
+++ applications/dashgo/src/pages/users/index.tsx
@@ -1,16 +1,16 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Tbody  } from '@chakra-ui/react';
-import { RiAddLine } from 'react-icons/ri';
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Td, Tbody, Checkbox, Text } from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
     return (
         <Box>
             <Header />
             <Flex w="100%" maxWidth={1480} mx="auto" px="6" mt="4">
-                <Sidebar/>
-
+                <Sidebar />
                 <Box flex="1" borderRadius={8} p="8" bg="gray.800">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
@@ -21,12 +21,35 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th>
-
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
                                 </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th></Th>
                             </Tr>
                         </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Gustavo Voigt</Text>
+                                        <Text fontSize="sm" color="gray.300">gustavo.voigt.dev@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril, 2021</Td>
+                                <Td>
+                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
                     </Table>
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
