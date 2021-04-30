5abbcb328149a73eef9ea118d49a71b2e29aab09
voigtito <voigt.gustavoh@gmail.com>
2021-04-30 07:42:36 -0300

    Responsive sidebar

--- applications/dashgo/src/components/Header/index.tsx
+++ applications/dashgo/src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react';
 import { Profile } from './Profile';
 import { NotificationsNav } from './NotificationsNav';
 import { SearchBox } from './SearchBox';
 import { Logo } from './Logo';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header() {
+
+    const { onOpen } = useSidebarDrawer();
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -22,6 +26,16 @@ export function Header() {
             px="6"
             align="center"
         >
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                />
+            )}
             <Logo />
             {isWideVersion && <SearchBox />}
             <Flex
