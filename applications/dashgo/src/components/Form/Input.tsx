9f41bd33111dbd135945be906026bb253c8f712f
voigtito <voigt.gustavoh@gmail.com>
2021-05-15 14:40:50 -0300

    Added yup with hook form for the login

--- applications/dashgo/src/components/Form/Input.tsx
+++ applications/dashgo/src/components/Form/Input.tsx
@@ -1,31 +1,34 @@
-import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { Input as ChakraInput, FormErrorMessage, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
 import { forwardRef, ForwardRefRenderFunction } from 'react'
+import {FieldError} from 'react-hook-form';
 
-interface InputProps extends ChakraInputProps{
-    name: string;
-    label?: string;
-
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError;
 }
 
-const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, ...rest}: InputProps, ref) => {
-    return (
-        <FormControl>
-        {/* FormControl is used for the label and input. So they wont receive the spacing from the Stack comp. */}
-        {/* All elements with action properties has _ in front of it. Like the _hover property */}
-            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput
-              name={name}
-              id={name}
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{ bgColor: 'gray.900' }}
-              size="lg"
-              ref={ref}
-              {...rest}
-            />
-          </FormControl>
-    )
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }: InputProps, ref) => {
+  return (
+    <FormControl isInvalid={!!error}>
+      {/* FormControl is used for the label and input. So they wont receive the spacing from the Stack comp. */}
+      {/* All elements with action properties has _ in front of it. Like the _hover property */}
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bgColor: 'gray.900' }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      
+    </FormControl>
+  )
 }
 
 export const Input = forwardRef(InputBase);
--- applications/dashgo/src/pages/index.tsx
+++ applications/dashgo/src/pages/index.tsx
@@ -1,15 +1,24 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 interface SignInFormData {
   email: string;
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória")
+})
+
 export default function Home() {
 
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (data, event) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
@@ -20,8 +29,8 @@ export default function Home() {
     <Flex w="100vw0" h="100vh" align="center" justify="center">
       <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" borderRadius={8} flexDir="column" onSubmit={handleSubmit(handleSignIn)}>
         <Stack spacing="4">
-          <Input type="email" name="email" label="E-mail" {...register('email')}/>
-          <Input type="password" name="password" label="Senha" {...register('password')}/>
+          <Input type="email" error={formState.errors.email} name="email" label="E-mail" {...register('email')}/>
+          <Input type="password" error={formState.errors.password} name="password" label="Senha" {...register('password')}/>
         </Stack>
         <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>Entrar</Button>
       </Flex>
