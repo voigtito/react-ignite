9ece3f3ea8d60e88cc8f8349b65fc1aceb41ba88
voigtito <voigt.gustavoh@gmail.com>
2021-05-21 08:12:14 -0300

    User creation api

--- applications/dashgo/src/pages/users/create.tsx
+++ applications/dashgo/src/pages/users/create.tsx
@@ -7,6 +7,10 @@ import Link from 'next/link';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
+import { useRouter } from 'next/dist/client/router';
 
 interface CreateUserFormData {
     name: string;
@@ -23,14 +27,29 @@ const createUserFromSchema = yup.object().shape({
 })
 
 export default function UserCreate() {
+    const router = useRouter();
+
+    const createUser = useMutation( async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date(),
+            }
+        })
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    })
 
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFromSchema)
     })
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(values)
+        await createUser.mutateAsync(values);
+        router.push('/users');
     }
 
     return (
