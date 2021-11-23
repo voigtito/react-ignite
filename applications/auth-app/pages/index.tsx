4fa28adfc30b217908e82d4a4cfb3c2eaecd1ef2
voigtito <voigt.gustavoh@gmail.com>
2021-11-23 13:47:24 -0300

    High order function for SSR redirect

--- applications/auth-app/pages/index.tsx
+++ applications/auth-app/pages/index.tsx
@@ -1,6 +1,9 @@
+import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
 
@@ -27,3 +30,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = withSSRGuest( async (context) => {
+  return {
+    props: {}
+  }
+});
